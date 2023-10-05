e52700b1c1982cc83ce131b4d8abd63f46869207
prince-bugingo <princebugingolearn.com>
2023-10-05 14:35:42 -0700

    ff

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/**
+ * main - Aprogram that prints a line with puts function
+ * return: 0
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");return (0);
+}
